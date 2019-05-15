b4d5c2ba2722df11bfde85b096a1c8ee82bbb566
kamild160 <293206@uwr.edu.pl>
2019-05-15 18:51:15 +0200

    Update zad1li2.cs

--- zad1li2.cs
+++ zad1li2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,27 +9,27 @@ namespace ConsoleApp5
 
     class Lista
     {
-        int [] tab ;
-        
+        int[] tab;
+
 
         public Lista(int k)
-        {   
+        {
             tab = new int[k];
             Random randNum = new Random();
-            for (int i=0;i<tab.Length;i++)
+            for (int i = 0; i < tab.Length; i++)
             {
                 tab[i] = randNum.Next(0, 100);
             }
 
         }
 
-       public Lista()
+        public Lista()
         {
-           
+
 
             Random rnd = new Random();
-            int r=rnd.Next(1,5);
-            int [] tab1;
+            int r = rnd.Next(1, 5);
+            int[] tab1;
 
             tab1 = new int[r];
             for (int i = 0; i < tab1.Length; i++)
@@ -41,37 +41,89 @@ namespace ConsoleApp5
 
         public override string ToString()
         {
-            
-            String lista="{ ";
-            for ( int i=0; i< tab.Length - 1;i++)
+
+            String lista = "{ ";
+            for (int i = 0; i < tab.Length - 1; i++)
             {
-                lista += tab[i]+ " ";
+                lista += tab[i] + " ";
             }
-           
+
             lista += "}";
             return lista;
         }
 
     }
 
-    class Lista1:Lista
+    public class Lista1 : Lista, IComparable<Lista2>
+    
     {
-        IComparable<>;
+        public Lista1(int size) : base(size) { }
+        public Lista1() : base() { }
+        // pokazanie ze ta klasa ma dziedziczyć konstruktory z klasy Lista
+        public int CompareTo(Lista1 that)
+        {
+            if (that == this) 
+                return 0;
 
+            else if (that == null) 
+                return 1;
+           
+             for (int i = 0; i < this.tab.Count && i < that.tab.Count; i++)
+            {
+                if (this.tab[i] > that.tab[i])
+
+                    return 1;
+
+                else if (this.tab[i] < that.tab[i])
+
+                    return -1;
+            }
+
+
+            return that.tab.Count > this.tab.Count ? -1 : 1;
+        }
+
+        public int CompareTo(Lista2 other)
+        {
+            throw new NotImplementedException();
+        }
     }
-
-    class Lista2:Lista
+    public class Lista2 : Lista, IComparable<Lista2>
     {
+        public Lista2(int size) : base(size) { }
 
+        public Lista2() : base() { }
+
+        public int CompareTo(Lista2 that)
+        {
+
+            if (this.tab.Count > that.tab.Count)
+
+                return 1;
+
+            else if (this.tab.Count < that.tab.Count)
+
+                return -1;
+
+            for (int i = 0; i < that.tab.Count && i < this.tab.Count; i++)
+            {
+                if (this.tab[i] > that.tabi])
+                   
+                     return 1;
+
+                else if (this.tab[i] < that.tab[i])
+                   
+                     return -1;
+            }
+        }
     }
-
 
     class Program
     {
         static void Main(string[] args)
         {
             Lista lista = new Lista(5);
-            
+
             Console.Write(lista);
             Console.ReadKey();
 
